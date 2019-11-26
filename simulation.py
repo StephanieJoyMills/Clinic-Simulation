e84c7047ff99b42c1d89f3bd4772b2cc3085dc33
StephanieJ10 <mills.stephanie.j@gmail.com>
2019-11-26 18:43:57 -0500

    add in print_time

--- simulation.py
+++ simulation.py
@@ -1,5 +1,6 @@
 import random
 import simpy
+import math
 from functools import partial, wraps
 
 RANDOM_SEED = 720
@@ -41,6 +42,50 @@ timing = {
     "lab_out": [[16, 10, 0], [12, 0, 0], [8, 0, 0]],
 }
 
+staff_schedule = {
+    # 8-12, 12-16, 16-20 hours
+    "doctor": [1, 1, 1],
+    "nurse": [2, 2, 2],
+    "imaging_tech": [1, 1, 1],
+    "lab_tech": [1, 1, 1],
+    "registration": [1, 1, 1],
+}
+
+costs = {
+    "doctor": 200,
+    "nurse": 100,
+    "imaging_tech": 90,
+    "lab_tech": 80,
+    "registration": 40
+}
+
+hourly_room_cost = {
+    "ED": 500,
+    "imaging": 400,
+    "lab": 300,
+    "registration": 50
+}
+
+capital_room_cost = {
+    "ED": 200000,
+    "imaging": 800000,
+    "lab": 180000,
+    "registration": 30000
+}
+
+# if someone from ER gets referred to something else
+mod_refferal = {
+    "imaging": 0.15,
+    "lab": 0.2,
+    "dep": 0.65
+}
+
+ser_refferal = {
+    "imaging": 0.2,
+    "lab": 0.25,
+    "dep": 0.55
+}
+
 
 class Patient(object):
     def __init__(self, env, num, priority, purpose, prob_balking, reneging_threshold):
@@ -68,6 +113,8 @@ class ED(object):
     def __init__(self, env):
         self.env = env
         self.room = simpy.resources.resource.PriorityResource(env, 4)
+        self.nurse = simpy.resources.resource.PriorityResource(env, 2)
+        self.doctor = simpy.resources.resource.PriorityResource(env, 2)
 
 
 # priority resource
@@ -75,6 +122,7 @@ class Imaging(object):
     def __init__(self, env):
         self.env = env
         self.station = simpy.resources.resource.PriorityResource(env, 1)
+        self.tech = simpy.resources.resource.PriorityResource(env, 1)
 
     def service(self, patient):
         service_time = random.triangular(8, 20, 12)
@@ -102,16 +150,7 @@ def patient(env, patient, registration, ed, imaging, lab):
             print('Patient {} is balking'.format(patient.id))
         else:
             print('Patient is not balking')
-            # Register nce finished registering
-            with registration.desk.request() as request:
-                yield request
-                print("Patient %s enters the registration at %.2f." %
-                      (patient.id, env.now))
-                yield env.process(registration.service(patient.id))
-                print("Patient %s finished registration service. Leaving lab at %.2f." %
-                      (patient.id, env.now))
-            # patient.reneging_threshold = patient.reneging_threshold + env.now
-
+            waitForRegistration(patient)
             with lab.station.request(priority=patient.priority) as request:
                 results = yield request | env.timeout(patient.reneging_threshold)
                 if request in results:
@@ -125,7 +164,58 @@ def patient(env, patient, registration, ed, imaging, lab):
                         print("Patient %s finished lab service. Leaving lab at %.2f." %
                               (patient.id, env.now))
                 else:
-                    print('Patient {} reneged'.format(patient.id))
+                    print('Patient {} reneged from lab'.format(patient.id))
+    if (patient.purpose == "img_out"):
+        if (imaging.station.count >= 5 and patient.prob_balking > random.randrange(0, 1)):
+            print('Patient {} is balking'.format(patient.id))
+        else:
+            print('Patient is not balking')
+            waitForRegistration(patient)
+            with imaging.station.request(priority=patient.priority) as request:
+                results = yield request | env.timeout(patient.reneging_threshold)
+                if request in results:
+                    print("Patient %s enters the imaging at %.2f." %
+                          (patient.id, env.now))
+                    with imaging.tech.request(priority=patient.priority) as request:
+                        yield request
+                        print("Imaging tech has arrived for service of patient {} at {}".format(
+                            patient.id, env.now))
+                        yield env.process(imaging.service(patient.id))
+                        print("Patient %s finished imaging service. Leaving imaging at %.2f." %
+                              (patient.id, env.now))
+                else:
+                    print('Patient {} reneged from imaging'.format(patient.id))
+    if (patient.purpose == "em_mod"):
+        if (ED.room.count >= 5 and patient.prob_balking > random.randrange(0, 1)):
+            print('Patient {} is balking'.format(patient.id))
+        else:
+            print('Patient is not balking')
+            waitForRegistration(patient)
+
+            with imaging.station.request(priority=patient.priority) as request:
+                results = yield request | env.timeout(patient.reneging_threshold)
+                if request in results:
+                    print("Patient %s enters the imaging at %.2f." %
+                          (patient.id, env.now))
+                    with imaging.tech.request(priority=patient.priority) as request:
+                        yield request
+                        print("Imaging tech has arrived for service of patient {} at {}".format(
+                            patient.id, env.now))
+                        yield env.process(imaging.service(patient.id))
+                        print("Patient %s finished imaging service. Leaving imaging at %.2f." %
+                              (patient.id, env.now))
+                else:
+                    print('Patient {} reneged from imaging'.format(patient.id))
+
+
+def waitForRegistration(patient):
+    with registration.desk.request() as request:
+        yield request
+        print("Patient %s enters the registration at %.2f." %
+              (patient.id, env.now))
+        yield env.process(registration.service(patient.id))
+        print("Patient %s finished registration service. Leaving registration at %.2f." %
+              (patient.id, env.now))
 
 
 def convert_time(time):
@@ -148,6 +238,44 @@ def convert_time(time):
     return [weekIndex, dayIndex]
 
 
+def print_time(env):
+    # env.now in min
+    # 105517.00.
+    # Week: Day:   Hour:   Min:
+
+    # time = env.now
+    time = 10140
+    week_min = 10080
+    modWeek = math.modf(time / week_min)
+    print(modWeek)
+    week = modWeek[1]
+    day_min = 1440
+    time = modWeek[0] * week_min
+    modDay = math.modf(time / day_min)
+    day = modDay[1]
+    time = modDay[0] * day_min
+    modHour = math.modf(time / 60)
+    hour = modHour[1]
+    minu = Math.round(modHour[0] / time)
+
+    # min_in_week = time - week * week_min
+    # print(min_in_week)
+    # day_min = 1440
+    # day = min_in_week % day_min
+    # min_in_day = min_in_week - day * day_min
+    # hour_min = 60
+    # hour = min_in_day % hour_min
+    # min_in_hour = min_in_day - hour * hour_min
+    # minu = min_in_hour
+    time = {
+        "week": week,
+        "day": day,
+        "hour": hour,
+        "minu": minu
+    }
+    print(time)
+
+
 def setup(env, registration, ed, imaging, lab):
     """Keep creating workers approx. every ``IAT`` minutes +-1 IAT_range ."""
     global patients_arrived
@@ -166,7 +294,9 @@ def setup(env, registration, ed, imaging, lab):
                                                                                  ][now[1]] + env.now
 
     while True:
+
         minTime = 99999
+        # Change shift of staff based on time on new days re-start
         for key, value in patient_timeouts.items():
             if value != None and value < minTime:
                 minTime = value
@@ -193,17 +323,19 @@ def setup(env, registration, ed, imaging, lab):
         patients_arrived += 1
 
 
-# Create an environment
-random.seed(RANDOM_SEED)
-env = simpy.Environment()
+# # Create an environment
+# random.seed(RANDOM_SEED)
+# env = simpy.Environment()
 
-# Initialize
-registration = Registration(env)
-ed = ED(env)
-imaging = Imaging(env)
-lab = Lab(env)
-print(lab.station.__dict__)
+# # Initialize
+# registration = Registration(env)
+# ed = ED(env)
+# imaging = Imaging(env)
+# lab = Lab(env)
+# print(lab.station.__dict__)
 
-# Set-up and Execute!
-env.process(setup(env, registration, ed, imaging, lab))
-env.run(until=SIM_TIME)
+# # Set-up and Execute!
+# env.process(setup(env, registration, ed, imaging, lab))
+# env.run(until=SIM_TIME)
+
+print_time(None)
