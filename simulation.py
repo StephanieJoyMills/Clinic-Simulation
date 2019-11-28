9cf6bab694c8c32e9450e795c3a5439f416c4e43
StephanieJ10 <mills.stephanie.j@gmail.com>
2019-11-27 21:41:24 -0500

    TODO - switch from IAT to arrival rate

--- simulation.py
+++ simulation.py
@@ -33,7 +33,7 @@ reneging = {
     "lab_out": [0, 0],
 }
 
-timing = {
+arrival_times = {
     "em_ser": [[1.5, 1, 1], [1.5, 1, 1], [1.5, 1, 0.5]],
     "em_mod": [[3, 2, 2], [3, 2, 1], [3, 1, 0.5]],
     "img_in": None,
@@ -157,99 +157,107 @@ class Lab(object):
         print("Lab service started for patient %s." % (patient))
 
 
-def patient(env, patient, registration, ed, imaging, lab):
-    # print(patient.__dict__)
+def patient(env, patient, registration, ED, imaging, lab):
+    print(patient.__dict__)
     if (patient.purpose == "lab_out"):
         if (lab.station.count >= 5 and patient.prob_balking > random.randrange(0, 1)):
-            print('Patient {} is balking'.format(patient.id))
+            print('Patient {} is balking from lab at {}'.format(
+                patient.id,  get_time(env)))
         else:
-            print('Patient is not balking')
-            waitForRegistration(patient)
+            print("here")
+            waitForRegistration(registration, patient)
             with lab.station.request(priority=patient.priority) as request:
                 results = yield request | env.timeout(patient.reneging_threshold)
                 if request in results:
-                    print("Patient %s enters the lab at %.2f." %
-                          (patient.id, env.now))
+                    print("Patient {} enters the lab at {}.".format(
+                        patient.id, get_time(env)))
                     with lab.tech.request(priority=patient.priority) as request:
                         yield request
-                        print("Lab tech has arrived for service of patient {} at {}".format(
-                            patient.id, env.now))
+                        print("Lab tech has arrived for service of patient {} at {}.".format(
+                            patient.id, get_time(env)))
                         yield env.process(lab.service(patient.id))
-                        print("Patient %s finished lab service. Leaving lab at %.2f." %
-                              (patient.id, env.now))
+                        print("Patient {} finished lab service at {}.".format(
+                            patient.id, get_time(env)))
                 else:
                     print('Patient {} reneged from lab'.format(patient.id))
     if (patient.purpose == "img_out"):
         if (imaging.station.count >= 5 and patient.prob_balking > random.randrange(0, 1)):
-            print('Patient {} is balking'.format(patient.id))
+            print('Patient {} is balking from imaging at {}'.format(
+                patient.id,  get_time(env)))
         else:
-            print('Patient is not balking')
-            waitForRegistration(patient)
+            waitForRegistration(registration, patient)
             with imaging.station.request(priority=patient.priority) as request:
                 results = yield request | env.timeout(patient.reneging_threshold)
                 if request in results:
-                    print("Patient %s enters the imaging at %.2f." %
-                          (patient.id, env.now))
+                    print("Patient {} enters the imaging at {}.".format(
+                        patient.id, get_time(env)))
                     with imaging.tech.request(priority=patient.priority) as request:
                         yield request
                         print("Imaging tech has arrived for service of patient {} at {}".format(
-                            patient.id, env.now))
+                            patient.id, get_time(env)))
                         yield env.process(imaging.service(patient.id))
-                        print("Patient %s finished imaging service. Leaving imaging at %.2f." %
-                              (patient.id, env.now))
+                        print("Patient {} finished imaging service at {}.".format
+                              (patient.id, get_time(env)))
                 else:
                     print('Patient {} reneged from imaging'.format(patient.id))
     if (patient.purpose == "em_mod"):
         if (ED.room.count >= 5 and patient.prob_balking > random.randrange(0, 1)):
-            print('Patient {} is balking'.format(patient.id))
+            print('Patient {} is balking from ED at {}'.format(
+                patient.id,  get_time(env)))
         else:
-            print('Patient is not balking')
-            waitForRegistration(patient)
+            waitForRegistration(registration, patient)
 
             with imaging.station.request(priority=patient.priority) as request:
                 results = yield request | env.timeout(patient.reneging_threshold)
                 if request in results:
-                    print("Patient %s enters the imaging at %.2f." %
-                          (patient.id, env.now))
+                    print("Patient {} enters the ED at {}.".format
+                          (patient.id, get_time(env)))
                     with imaging.tech.request(priority=patient.priority) as request:
                         yield request
                         print("Imaging tech has arrived for service of patient {} at {}".format(
-                            patient.id, env.now))
+                            patient.id, get_time(env)))
                         yield env.process(imaging.service(patient.id))
-                        print("Patient %s finished imaging service. Leaving imaging at %.2f." %
-                              (patient.id, env.now))
+                        print("Patient %s finished ED service at {}.".format
+                              (patient.id, get_time(env)))
                 else:
                     print('Patient {} reneged from imaging'.format(patient.id))
 
 
 def waitForRegistration(registration, patient):
+    print('Patient {} enters line for registration at {}'.format(
+        patient.id,  get_time(env)))
     with registration.desk.request() as request:
         yield request
-        print("Patient %s enters the registration at %.2f." %
-              (patient.id, env.now))
-        yield env.process(registration.service(patient.id))
-        print("Patient %s finished registration service. Leaving registration at %.2f." %
-              (patient.id, env.now))
+        print('Patient {} enters the registration at {}'.format(
+            patient.id,  get_time(env)))
+        with registration.clerk.request() as request:
+            yield request
+            print("Registration clerk has arrived for service of patient {} at {}".format(
+                patient.id, env.now))
+            yield env.process(registration.service(patient.id))
+            print("Patient {} finished registration service at {}".format(
+                patient.id, env.now))
 
 
-def convert_time(time):
-    tot_hours = time / 60 / 60
-    week_hours = tot_hours % 84
-    if (week_hours < 60):
-        weekIndex = 0
-    elif (week_hours > 72):
-        weekIndex = 2
+def get_index_by_time(time):
+    hour = time["hour"]
+    if (hour >= 8 and hour < 12):
+        day_index = 0
+    elif (hour >= 12 and hour < 16):
+        day_index = 1
+    elif (hour >= 16 and hour < 20):
+        day_index = 2
     else:
-        weekIndex = 1
+        day_index = None
 
-    day_hours = week_hours % 12
-    if (day_hours < 4):
-        dayIndex = 0
-    elif (day_hours > 8):
-        dayIndex = 2
+    week_day = time["day"]
+    if (week_day <= 5):
+        week_day_index = 0
+    if (week_day == 6):
+        week_day_index = 1
     else:
-        dayIndex = 1
-    return [weekIndex, dayIndex]
+        week_day_index = 2
+    return {"week_day_index": week_day_index, "day_index": day_index}
 
 
 def get_time(env):
@@ -273,10 +281,10 @@ def get_time(env):
         "hour": hour,
         "minu": minu
     }
-    print(time)
+    return time
 
 
-def setup(env, registration, ed, imaging, lab):
+def setup(env):
     """Keep creating workers approx. every ``IAT`` minutes +-1 IAT_range ."""
     global patients_arrived
     patient_timeouts = {
@@ -287,48 +295,53 @@ def setup(env, registration, ed, imaging, lab):
         "lab_in": None,
         "lab_out": None,
     }
+
+
+# arrival_times = {
+#     "em_ser": [[1.5, 1, 1], [1.5, 1, 1], [1.5, 1, 0.5]],
+#     "em_mod": [[3, 2, 2], [3, 2, 1], [3, 1, 0.5]],
+#     "img_in": None,
+#     "img_out": [[12, 8, 0], [10, 5, 0], [5, 2, 0]],
+#     "lab_in": None,
+#     "lab_out": [[16, 10, 0], [12, 0, 0], [8, 0, 0]],
+# }
     for key in patient_timeouts:
         if patient_timeouts[key] == None:
-            now = convert_time(env.now)
-            patient_timeouts[key] = None if timing[key] == None else timing[key][now[0]
-                                                                                 ][now[1]] + env.now
-
+            time = get_time(env)
+            time_indexes = get_index_by_time(time)
+            patient_timeouts[key] = None if arrival_times[key] == None else arrival_times[key][time_indexes["week_day_index"]
+                                                                                               ][time_indexes["day_index"]] + env.now
     shift_change = [False, False, False]
 
     while True:
-
         time = get_time(env)
-        hour = time["hour"]
-        if (hour > 20):
-            print("Clinic is now closed.".format(time))
-            shift_change = [False, False, False]
-            # clinic closes for 12 hours
-            yield env.timeout(60 * 12)
-            break
+        time_indexes = get_index_by_time(time)
 
-        if (hour > 8 and hour < 12 and shift_change[0] == False):
+        if (time_indexes["day_index"] == 0 and shift_change[0] == False):
+            print("First shift staffing at {}".format(time))
             registration = Registration(env, staff_schedule["registration"][0])
             ed = ED(env, staff_schedule["nurse"]
                     [0], staff_schedule["doctor"][0])
             imaging = Imaging(env, staff_schedule["imaging_tech"][0])
             lab = Lab(env, staff_schedule["lab_tech"][0])
-            shift_change[0] == True
+            shift_change[0] = True
 
-        elif (hour > 12 and hour < 16 and shift_change[1] == False):
+        elif (time_indexes["day_index"] == 1 and shift_change[1] == False):
+            print("Second shift change at {}".format(time))
             registration = Registration(env, staff_schedule["registration"][1])
             ed = ED(env, staff_schedule["nurse"]
                     [1], staff_schedule["doctor"][1])
             imaging = Imaging(env, staff_schedule["imaging_tech"][1])
             lab = Lab(env, staff_schedule["lab_tech"][1])
-            shift_change[1] == True
-
-        elif (hour > 16 and hour < 20 and shift_change[2] == False):
+            shift_change[1] = True
+        elif (time_indexes["day_index"] == 2 and shift_change[2] == False):
+            print("Third shift change at {}".format(time))
             registration = Registration(env, staff_schedule["registration"][2])
             ed = ED(env, staff_schedule["nurse"]
                     [2], staff_schedule["doctor"][2])
             imaging = Imaging(env, staff_schedule["imaging_tech"][2])
             lab = Lab(env, staff_schedule["lab_tech"][2])
-            shift_change[2] == True
+            shift_change[2] = True
 
         minTime = 99999
         # Change shift of staff based on time on new days re-start
@@ -336,11 +349,11 @@ def setup(env, registration, ed, imaging, lab):
             if value != None and value < minTime:
                 minTime = value
                 next_timeout = key
-
-        yield env.timeout(patient_timeouts[next_timeout])
-
+        print(env.now)
+        yield env.timeout(patient_timeouts[next_timeout] - env.now)
+        print(env.now)
         priority = (
-            2 if next_timeout == "em_ser" else 1 if next_timeout == "em_mod" else None
+            2 if next_timeout == "em_ser" else 1 if next_timeout == "em_mod" else 0
         )
 
         prob_balking = balking[next_timeout]
@@ -354,24 +367,42 @@ def setup(env, registration, ed, imaging, lab):
                               next_timeout, prob_balking, reneging_threshold)
 
         env.process(patient(env, new_patient, registration, ed, imaging, lab))
-        patient_timeouts[next_timeout] = timing[key][now[0]][now[1]] + env.now
+
+        if (time_indexes["day_index"] == None):
+            print("Clinic is now closed".format(time))
+            shift_change = [False, False, False]
+            # clinic closes for 12 hours
+            yield env.timeout(60 * 12)
+            time = get_time(env)
+            time_indexes = get_index_by_time(time)
+            print("Clinic is now open!".format(time))
+        # time = get_time(env)
+        # time_indexes = get_index_by_time(time)
+        print(patient_timeouts[next_timeout])
+
+        patient_timeouts[next_timeout] = arrival_times[next_timeout][time_indexes["week_day_index"]
+                                                                     ][time_indexes["day_index"]] + env.now
+        print(arrival_times[next_timeout][time_indexes["week_day_index"]
+                                          ][time_indexes["day_index"]])
+        print(env.now)
+        print(patient_timeouts[next_timeout])
         patients_arrived += 1
 
 
 # Create an environment
 random.seed(RANDOM_SEED)
 env = simpy.Environment(initial_time=60*8)
+print(env.now)
+print(get_time(env))
 
 # Initialize
-registration = Registration(env, staff_schedule["registration"][2])
-ed = ED(env, staff_schedule["nurse"]
-        [2], staff_schedule["doctor"][2])
-imaging = Imaging(env, staff_schedule["imaging_tech"][2])
-lab = Lab(env, staff_schedule["lab_tech"][2])
-print(lab.station.__dict__)
-
+# registration = Registration(env, staff_schedule["registration"][2])
+# ed = ED(env, staff_schedule["nurse"]
+#         [2], staff_schedule["doctor"][2])
+# imaging = Imaging(env, staff_schedule["imaging_tech"][2])
+# lab = Lab(env, staff_schedule["lab_tech"][2])
 # Set-up and Execute!
-env.process(setup(env, registration, ed, imaging, lab))
-env.run(until=SIM_TIME)
+env.process(setup(env))
+env.run(until=1000)
 
 # print_time(None)
